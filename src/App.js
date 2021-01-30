e4c1373d43a38eeb23e1beda1d2f3bf490c6d4da
chaejonghun <ace1373@naver.com>
2021-01-31 00:18:06 +0900

    520페이지. 사소한  에러 존재함

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import CounterContainer from "./container/CounterContainer";
 import SampleContainer from "./container/SampleContainer";
 
 function App() {
   return (
     <div>
-      <SampleContainer />
+      <CounterContainer />
     </div>
   );
 }
---
+++ src/lib/createRequestSaga.js
@@ -0,0 +1,24 @@
+import { call, put } from "redux-saga/effects";
+import { startLoading, finishLoading } from "../modules/loading";
+
+export default function createRequestSaga(type, request) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  return function* (action) {
+    yield put(startLoading(type));
+    try {
+      yield put({
+        type: SUCCESS,
+        payload: Response.data,
+      });
+    } catch (e) {
+      yield put({
+        type: FAILURE,
+        payload: e,
+        error: true,
+      });
+    }
+    yield put(finishLoading(type));
+  };
+}
